3b8fd34ff33909e621165b4c5c546ec07384d45c
Golodhrim <holidayninjastaff@gmail.com>
2021-06-07 22:59:25 -0400

    removed unused files

--- src/index.js
+++ src/index.js
@@ -2,18 +2,10 @@ import React from 'react';
 import LevelState from './context/LevelState';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<LevelState>
-			<App />
-		</LevelState>
-	</React.StrictMode>,
+	<LevelState>
+		<App />
+	</LevelState>,
 	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
